634fa1c91b922aac142b9b0bf9d7c3e96566fe83
jhpark <jhpark@gaonpf.com>
2024-06-19 14:04:47 +0900

    'update_file'

---
+++ 5(금)보충/forecast/test_client.py
@@ -0,0 +1,27 @@
+import asyncio
+import datetime
+import json
+import random
+
+import websockets
+
+
+async def send_data():
+    uri = 'ws://localhost:1111'
+    async with websockets.connect(uri) as websocket:
+        while True:
+            data = {
+                'tagname': 'TagName3',  # TagName1, TagName2, TagName3 중 하나를 선택
+                'values': round(random.random(),2),  # 랜덤 데이터 생성
+                'timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                'quality': 'good'
+            }
+            await websocket.send(json.dumps(data))
+            print(data['values'])
+            await asyncio.sleep(random.uniform(1,10))
+
+
+            response = await websocket.recv()
+            print(f"Received: {response}")
+
+asyncio.get_event_loop().run_until_complete(send_data())
